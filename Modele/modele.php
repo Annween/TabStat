02729a0ccd181a91e632cd1992a39e5e5d975bf0
Annween <49524082+Annween@users.noreply.github.com>
2020-01-09 14:03:09 +0100

    Création de fonctions

--- Modele/modele.php
+++ Modele/modele.php
@@ -1,37 +1,59 @@
 <?php
-
 try
-	{
-	    $db = new PDO('mysql:host=localhost;dbname=tabstat;charset=utf8', 'root', '');
-	    echo "Connexion réussie";
-	}
-catch(Exception $e)
-	{
-	    die('Erreur : '.$e->getMessage());
-	}
-
-	
-
-$req = $db->query('SELECT name AS PROJETS, id FROM mantis_project_table');
-	$req2 = $db->query('SELECT name AS PROJETS, id FROM mantis_project_table');
-	
-function comptage($id) { $req1 = query("SELECT project_id, tranche_delai, count(*) as count
-							FROM (SELECT id, project_id, DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) as DELAI, 
-										CASE WHEN DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <=7 THEN 'Moins 7'
-											WHEN DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <=14 THEN '8-14'
-											WHEN DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <=21 THEN '15-21'
-											WHEN DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <=28 THEN '22-28'
-											ELSE 'autre' END as tranche_delai
-									FROM mantis_bug_table 
-									WHERE status not in (80, 90) ) a
-					GROUP BY a.project_id, a.tranche_delai
-					WHERE a.project_id = $id");
-		return $req1;
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=tabstat;charset=utf8', 'root', '');
+    echo "Connexion réussie";
 }
-	
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+
+$reqProjectList = $bdd->query('SELECT id, name FROM mantis_project_table');
 
 
-	
+
+ function getLastWeek($database, $id)
+{
+  return $database->query('SELECT COUNT(*) AS nbLessWeek FROM `mantis_bug_table` WHERE `project_id`='.$id.' AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <= 7')->fetch();
+}
+
+function getFrom7To14($database, $id)
+{
+	return $database->query('SELECT COUNT(*) AS nbReq7To14 FROM `mantis_bug_table` WHERE `project_id`='.$id.' AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) > 7 AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <= 14')->fetch();
+}
+
+function getFrom15To21($database, $id)
+{
+	return $database->query('SELECT COUNT(*) AS nbReq15To21 FROM `mantis_bug_table` WHERE `project_id`='.$id.' AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) > 15 AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <= 21')->fetch();
+}
+
+function getFrom22To28($database, $id)
+{
+	return $database->query('SELECT COUNT(*) AS nbReq22To28 FROM `mantis_bug_table` WHERE `project_id`='.$id.' AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) > 22 AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <= 28')->fetch();
+}
+
+function getFrom29To90($database, $id)
+{
+	return $database->query('SELECT COUNT(*) AS nbReq29To90 FROM `mantis_bug_table` WHERE `project_id`='.$id.' AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) > 29 AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <= 90')->fetch();
+}
+
+function getFrom91To180($database, $id)
+{
+	return $database->query('SELECT COUNT(*) AS nbReq91To180 FROM `mantis_bug_table` WHERE `project_id`='.$id.' AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) > 90 AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) <= 180')->fetch();
+}
+
+function getMoreThan180($database, $id)
+{
+	return $database->query('SELECT COUNT(*) AS nbReqPlus180 FROM `mantis_bug_table` WHERE `project_id`='.$id.' AND DATEDIFF(NOW(),FROM_UNIXTIME(date_submitted)) > 180')->fetch();
+}
+
+function getTotal($database, $id)
+{
+	return $database->query('SELECT COUNT(*) AS nbReqTotal FROM `mantis_bug_table` WHERE `project_id`='.$id.'')->fetch();
+}
+
+
+
 
 ?>
-
--- Vue/accueil.php
+++ Vue/accueil.php
@@ -17,52 +17,87 @@
         <div class="contenu_onglets">
             <div class="contenu_onglet" id="contenu_onglet_Tab1">
               
-              <table id="Tab1">
+              <table>
               <tr>
-              <td><th><p>Projets</p></th></td>
-              <td><th><p>7 jours et moins</p></th></td>
-              <td><th><p>de 7 à 14 jours</p></th></td>
-              <td><th><p>de 15 à 21 jours</p></th></td>
-              <td><th><p>de 22 à 28 jours</p></th></td>
-              <td><th><p>de 29 à 90 jours</p></th></td>
-              <td><th><p>de 91 à 180 jours</p></th></td>
-              <td><th><p>plus de 180 jours</p></th></td>
-              <td><th><p>Total</p></th></td>
-              <td><th><p>Semaine S-1</p></th></td>
-              <td><th><p>Evolution</p></th></td>
+              <td width="10%><th">Projets</th></td>
+              <td width="10%><th">7 jours et moins</th></td>
+              <td width="10%><th">de 7 à 14 jours</th></td>
+              <td width="10%><th">de 15 à 21 jours</th></td>
+              <td width="10%><th">de 22 à 28 jours</th></td>
+              <td width="10%><th">de 29 à 90 jours</th></td>
+              <td width="10%><th">de 91 à 180 jours</th></td>
+              <td width="10%><th">plus de 180 jours</th></td>
+              <td width="10%><th">Total</th></td>
+              <td width="10%><th">Semaine S-1</th></td>
+              <td width="10%><th">Evolution</th></td>
               </tr>
 
           
             
             <?php
             
-            while ($donnees = $req->fetch())
-            
-            echo"<tr><td><br>{$donnees['PROJETS']}</td></tr>";
+			/* Iteration sur les projets, une ligne par projet */
+            while ($donnees = $reqProjectList->fetch())
+			{
+			
+			  $reqLessAWeek = getLastWeek($bdd, $donnees['id']);
+			  /* Recuperation du nombre d'occurence du projet en cours qui ont entre 7 jours et 14 jours */
+			  $req7To14 = getFrom7To14($bdd, $donnees['id']);
+			  /* Recuperation du nombre d'occurence du projet en cours qui ont entre 15 jours et 21 jours */
+			  $req15To21 = getFrom15To21($bdd, $donnees['id']);
+			  /* Recuperation du nombre d'occurence du projet en cours qui ont entre 22 jours et 28 jours */
+			  $req22To28 = getFrom22To28($bdd, $donnees['id']);
+			  /* Recuperation du nombre d'occurence du projet en cours qui ont entre 29 jours et 90 jours */
+			  $req29To90 = getFrom29To90($bdd, $donnees['id']);
+			  /* Recuperation du nombre d'occurence du projet en cours qui ont entre 91 jours et 180 jours */
+			  $req91To180 = getFrom91To180($bdd, $donnees['id']);
+			  /* Recuperation du nombre d'occurence du projet en cours qui ont plus de 180 jours */
+			  $reqPlus180 = getMoreThan180($bdd, $donnees['id']);
+			  /* Recuperation du total */
+			  $reqTotal = getTotal($bdd, $donnees['id']);
 
+			  $SMinusOne = (  ((int)$reqTotal['nbReqTotal']) - ((int)$reqLessAWeek['nbLessWeek']));
+
+              $diffEvo = (  ((int)$reqTotal['nbReqTotal'])- ((int)$SMinusOne));
+
+
+
+			  echo "<tr>";
+			    /* Inscription du nom du projet */
+			    echo "<td>".$donnees['name']."</td>";
+				/* Inscription du nombre de ticket de moins d'une semaine */
+				$buffData = $reqLessAWeek;
+				echo "<td>".$buffData['nbLessWeek']."</td>";
+				/* Inscription du nombre de ticket de 7 à 14 jours */
+				$buffData = $req7To14;
+				echo "<td>".$buffData['nbReq7To14']."</td>";
+				/* Inscription du nombre de ticket de 15 à 21 jours */
+				$buffData = $req15To21;
+				echo "<td>".$buffData['nbReq15To21']."</td>";
+				/* Inscription du nombre de ticket de 22 à 28 jours */
+				$buffData = $req22To28;
+				echo "<td>".$buffData['nbReq22To28']."</td>";
+				/* Inscription du nombre de ticket de 29 à 90 jours */
+				$buffData = $req29To90;
+				echo "<td>".$buffData['nbReq29To90']."</td>";
+				/* Inscription du nombre de ticket de 91 à 180 jours */
+				$buffData = $req91To180;
+				echo "<td>".$buffData['nbReq91To180']."</td>";
+				/* Inscription du nombre de ticket de plus de 180 jours */
+				$buffData = $reqPlus180;
+				echo "<td>".$buffData['nbReqPlus180']."</td>";
+				/* Inscription du nombre de ticket total */
+				$buffData = $reqTotal;
+				echo "<td>".$buffData['nbReqTotal']."</td>";
+				/* Inscription de la diff semaine -1 */
+				echo "<td>".$SMinusOne."</td>";
+				/* Inscription de la diff finale */
+				echo "<td>".$diffEvo."</td>";
+			  echo "</tr>\n";
+			} 
             
-          
             ?>
-            
-      
-
-
               </table>
-
-
-<table id="liste">
-    
-<?php while ($donnees2 = $req2->fetch())
-  {
-    comptage($donnees2['id']);
-echo '<tr><td>'.$donnees2['PROJETS'].'</td>';
-
-while ($list = $req1->fetch() ) 
-    { echo '<td>'.$list['count'].'</td></tr>' ;}
-  } ?>
-  </table>
-
-
 
             </div>
 
@@ -81,6 +116,3 @@ while ($list = $req1->fetch() )
 
 </body>
 </html>
-
-
-
