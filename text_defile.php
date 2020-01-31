a650870224835f9db1b2d3f3ad64bf1ef03280b9
Annween <49524082+Annween@users.noreply.github.com>
2020-01-31 15:51:52 +0100

    TEXTE DEFILANT

---
+++ text_defile.php
@@ -0,0 +1,24 @@
+echo "
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+
+	<title>Test</title>
+
+</head>
+
+<body>
+
+<div id="mon_texte">
+	
+<marquee direction='right'>COUCOU AKTO</marquee>
+
+</div>	
+
+</body>
+
+
+
+
+
+";
